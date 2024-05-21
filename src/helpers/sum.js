ab891c72375d43831c4f61cc5b393427ab1578fc
I-N-S-O-N <79045287+I-N-S-O-N@users.noreply.github.com>
2024-05-21 15:00:18 +0500

    init

---
+++ src/helpers/sum.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3];
+
+const value = 'hello';
+console.log(value);
+
+function sumNumbers() {
+  for (const iterator of numbers) {
+    console.log(iterator);
+  }
+  return 1;
+}
+sumNumbers();
