9fd1a3aa27eaa4599b98a128ce2fe334257c8e6d
I-N-S-O-N <79045287+I-N-S-O-N@users.noreply.github.com>
2024-05-21 15:22:54 +0500

    test script changed 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
+import React from 'react'
+import test, {   expect } from 'node:test';
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+// console.log("testing");
+
+// import test, { describe, expect } from 'node:test';
+// import { sum } from './sum';
+
+// describe('sum', function () {
+//   test('positive', () => {
+//     expect(sum(5, 5)).toBe(10);
+//   });
 // });
-console.log('testing');
